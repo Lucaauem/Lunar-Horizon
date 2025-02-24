abb681f91fe0fa2aff9bd51f4ef85b1a8dd5ba72
Luca.aue <luca-aussem@t-online.de>
2025-02-25 00:04:44 +0100

    Changed: Correct tile position

--- src/main/java/engine/graphics/tiles/Tile.java
+++ src/main/java/engine/graphics/tiles/Tile.java
@@ -5,6 +5,7 @@ import engine.GameWindow;
 import engine.graphics.Model;
 import engine.graphics.renderer.Renderer;
 import org.joml.Matrix4f;
+import org.joml.Vector2f;
 import org.joml.Vector4f;
 
 import static engine.objects.GameObject.DEFAULT_TILE_SIZE;
@@ -12,8 +13,10 @@ import static engine.objects.GameObject.DEFAULT_TILE_SIZE;
 public class Tile {
 	private final Tilemap tilemap;
 	private final Model model;
+	private final Vector2f position;
 
-	public Tile(Tilemap tilemap, int textureIndex) {
+	public Tile(Tilemap tilemap, int textureIndex, Vector2f position) {
+		this.position = position;
 		this.tilemap = tilemap;
 		Vector4f uv = tilemap.getUv(textureIndex);
 
@@ -30,8 +33,10 @@ public class Tile {
 
 		Matrix4f proj = new Matrix4f().ortho(0.0f, GameWindow.RESOLUTION.x, 0.0f, GameWindow.RESOLUTION.y, -1.0f, 1.0f);
 		Matrix4f view = Game.camera.getMatrix();
+		Matrix4f model = new Matrix4f().identity();
+		model.translate((this.position.x * DEFAULT_TILE_SIZE) + DEFAULT_TILE_SIZE, (this.position.y * DEFAULT_TILE_SIZE) + DEFAULT_TILE_SIZE, 0);
 
-		Matrix4f mvp = proj.mul(view);
+		Matrix4f mvp = proj.mul(view).mul(model);
 		Game.shader.setUniformMat4f("u_MVP", mvp);
 
 		Renderer.getInstance().draw(this.model.getVertexArray(), this.model.getIndexBuffer(), Game.shader);
--- src/main/java/engine/scenes/Scene.java
+++ src/main/java/engine/scenes/Scene.java
@@ -2,12 +2,14 @@ package engine.scenes;
 
 import engine.graphics.tiles.Tile;
 import engine.graphics.tiles.Tilemap;
+import org.joml.Vector2f;
 import util.FileHandler;
 import java.util.ArrayList;
 
 public class Scene {
 	private static final String SCENE_BASE_PATH = "src/main/assets/scenes/";
 	private static final String SCENE_EXTENSION = ".scene.bin";
+	private static final int SCENE_SIZE = 3;
 
 	private final Tilemap tilemap = new Tilemap();
 	private final Tile[] tiles;
@@ -16,15 +18,23 @@ public class Scene {
 		byte[] rawData = FileHandler.readBinary(SCENE_BASE_PATH + name + SCENE_EXTENSION);
 		ArrayList<Tile> tiles = new ArrayList<>();
 
-		for(byte b : rawData) {
-			tiles.add(this.generateTile(b));
+		Vector2f tilePosition = new Vector2f(0, SCENE_SIZE);
+		for (byte rawDatum : rawData) {
+			tiles.add(this.generateTile(rawDatum, new Vector2f(tilePosition.x, tilePosition.y)));
+
+			if(tilePosition.x++ > SCENE_SIZE) {
+				tilePosition.x = 0;
+				tilePosition.y--;
+			}
 		}
 
 		this.tiles = tiles.toArray(new Tile[0]);
 	}
 
-	public Tile generateTile(byte data) {
-		return new Tile(this.tilemap, 0);
+	public Tile generateTile(byte data, Vector2f position) {
+		if(data == 0) { return null; }
+
+		return new Tile(this.tilemap, 0, position);
 	}
 
 	public void render() {
