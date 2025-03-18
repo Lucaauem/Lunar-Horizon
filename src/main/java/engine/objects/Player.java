89b3d56ccb5fa41d8d0eba26ef928c15abfd9d7a
Luca.aue <luca-aussem@t-online.de>
2025-03-18 23:41:11 +0100

    Added: Player stats

--- src/main/java/engine/objects/Player.java
+++ src/main/java/engine/objects/Player.java
@@ -8,14 +8,33 @@ public class Player extends Entity {
 	private static final String PLAYER_TEXTURE_PATH = "sample.png";
 
 	private final Vector2f lastValidPosition = new Vector2f();
+	private int level = 1;
+	private int maxHealth = 10;
+	private int health = maxHealth;
+	private int attack = 2;
+	private int maxMagic = 15;
+	private int magic = maxMagic;
+	private int experience = 0;
 
 	public Player() {
 		super(PLAYER_TEXTURE_PATH, new Vector2f(0, 0));
 	}
 
-	public Vector2f getLastValidPosition() {
-		return lastValidPosition;
-	}
+	public Vector2f getLastValidPosition() { return lastValidPosition; }
+
+	public int getLevel() { return this.level;}
+
+	public int getHealth() { return this.health; }
+
+	public int getMaxHealth() { return this.maxHealth; }
+
+	public int getAttack() { return this.attack; }
+
+	public int getMagic() { return this.magic; }
+
+	public int getMaxMagic() { return this.magic; }
+
+	public int getExperience() { return this.experience; }
 
 	@Override
 	protected void onStepEnd() {
--- src/main/java/engine/ui/menu/PageContent.java
+++ src/main/java/engine/ui/menu/PageContent.java
@@ -1,5 +1,8 @@
 package engine.ui.menu;
 
+import engine.Game;
+
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +15,22 @@ public class PageContent {
 			));
 			case PLAYER_MENU -> new ArrayList<>(List.of(
 					new MenuItem("INV.", () -> { /* TODO */}),
-					new MenuItem("PARTY", () -> { /* TODO */ })
+					new MenuItem("STATS", PageContent::showStats)
 			));
 			case SYSTEM -> new ArrayList<>(List.of(
 					new MenuItem("EXIT", () -> System.exit(0))
 			));
 		};
+	}
+
+	// !TODO! Open real menu
+	private static void showStats() {
+		System.out.println("====== Stats ======");
+		System.out.println(MessageFormat.format("Level: {0}", Game.player.getLevel()));
+		System.out.println(MessageFormat.format("HP: {0}/{1}", Game.player.getHealth(), Game.player.getMaxHealth()));
+		System.out.println(MessageFormat.format("MP: {0}/{1}", Game.player.getMagic(), Game.player.getMaxMagic()));
+		System.out.println(MessageFormat.format("Attack: {0}", Game.player.getAttack()));
+		System.out.println(MessageFormat.format("EXP: {0}", Game.player.getExperience()));
+		System.out.println("===================");
 	}
 }
