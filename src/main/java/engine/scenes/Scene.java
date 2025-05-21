bc73e6e54808eefc68ec6c335b27e26ab7868e45
Luca.aue <luca-aussem@t-online.de>
2025-05-21 23:31:01 +0200

    Changed: Process for scene loading

--- src/main/java/engine/scenes/Scene.java
+++ src/main/java/engine/scenes/Scene.java
@@ -8,13 +8,12 @@ import engine.objects.trigger.Trigger;
 import org.joml.Vector2f;
 import org.joml.Vector2i;
 import util.FileHandler;
-import java.util.ArrayList;
 
 public class Scene {
 	private static final String SCENE_BASE_PATH = "src/main/assets/scenes/";
-	private static final String SCENE_EXTENSION = ".scene.bin";
-	private static final String TRIGGER_EXTENSION = ".trigger.bin";
-	private static final String CONFIG_EXTENSION = ".conf.bin";
+	private static final String TILES_EXTENSION = "tiles.bin";
+	private static final String TRIGGER_EXTENSION = "trigger.bin";
+	private static final String CONFIG_EXTENSION = "conf.bin";
 	private static final int SCENE_SIZE = 8;
 
 	private final Tilemap tilemap = new Tilemap();
@@ -23,18 +22,26 @@ public class Scene {
 	private final String name;
 
 	public Scene(String name) {
-		this.name = name;
+		this.name = name + "/";
 
-		byte[] rawTileData = FileHandler.readBinary(SCENE_BASE_PATH + name + SCENE_EXTENSION);
-		ArrayList<Tile> tiles = new ArrayList<>();
+		this.tiles = this.loadTiles();
+		this.loadConfig();
+		this.loadTrigger();
+	}
+
+	// region GENERATE TILES
+
+	// Tiles consist of 2 bytes.
+	// The first byte is the index in the tilemap (texture).
+	// The second byte modifies the tiles (mirroring, palette, collidable, ...).
+	private Tile[] loadTiles() {
+		byte[] rawTileData = FileHandler.readBinary(SCENE_BASE_PATH + this.name + TILES_EXTENSION);
+		Tile[] tiles = new Tile[SCENE_SIZE * SCENE_SIZE];
+
 		Vector2f tilePosition = new Vector2f(0, SCENE_SIZE);
-
-		// Tiles consist of 3 bytes.
-		// The first byte is the index in the tilemap (texture).
-		// The second byte modifies the visuals (mirroring, palette, ...).
-		// The third byte contains stauts information (e.g. collision, special behaviour, ...).
-		for(int i=0; i<rawTileData.length; i+=3) {
-			tiles.add(this.generateTile(rawTileData[i], rawTileData[i + 1], rawTileData[i + 2], new Vector2f(tilePosition.x, tilePosition.y)));
+		int tileIndex = 0;
+		for(int i=0; i<rawTileData.length; i+=2) {
+			tiles[tileIndex++] = (this.generateTile(rawTileData[i], rawTileData[i + 1], new Vector2f(tilePosition)));
 
 			if(++tilePosition.x >= SCENE_SIZE) {
 				tilePosition.x = 0;
@@ -42,29 +49,51 @@ public class Scene {
 			}
 		}
 
-		this.tiles = tiles.toArray(new Tile[0]);
+		return tiles;
+	}
 
-		byte[] rawConfigData = FileHandler.readBinary(SCENE_BASE_PATH + name + CONFIG_EXTENSION);
+	// Modifier byte:
+	// 76543210
+	// ||||||||
+	// |||||||+- 1st bit for the number of rotations
+	// ||||||+-- 2nd bit for the number of rotations
+	// |||||+---
+	// ||||+----
+	// |||+-----
+	// |+-------
+	// +-------- Can be walked on/through
+	public Tile generateTile(byte index, byte modifier, Vector2f position) {
+		Tile tile = new Tile(this.tilemap, index, position, (modifier & 0b10000000) != 0);
+		tile.rotate90Deg(modifier & 0b00000011);
+
+		return tile;
+	}
+
+	private void loadConfig() {
+		byte[] rawConfigData = FileHandler.readBinary(SCENE_BASE_PATH + this.name + CONFIG_EXTENSION);
+
 		for(int i=0; i<rawConfigData.length; i+=3) {
-			this.loadConfig(rawConfigData[i], rawConfigData[i + 1], rawConfigData[i + 2]);
-		}
-
-		byte[] rawTriggerData = FileHandler.readBinary(SCENE_BASE_PATH + name + TRIGGER_EXTENSION);
-
-		// Tiles consist of 4 bytes.
-		// The first byte is the x-index of the tile.
-		// The second byte is the y-index of the tile.
-		// The third byte is the type of trigger.
-		// The fourth byte is parameter data for the trigger.
-		for(int i=0; i<rawTriggerData.length; i+=4) {
-			this.createTrigger(rawTriggerData[i], rawTriggerData[i + 1], rawTriggerData[i + 2], rawTriggerData[i + 3]);
+			this.readConfig(rawConfigData[i], rawConfigData[i + 1], rawConfigData[i + 2]);
 		}
 	}
 
-	private void loadConfig(byte type, byte param1, byte param2) {
+	private void readConfig(byte type, byte param1, byte param2) {
 		if(type == 0) {
 			this.spawn.x = param1;
 			this.spawn.y = param2;
+		}
+	}
+
+	// Tiles consist of 4 bytes.
+	// The first byte is the x-index of the tile.
+	// The second byte is the y-index of the tile.
+	// The third byte is the type of trigger.
+	// The fourth byte is parameter data for the trigger.
+	private void loadTrigger() {
+		byte[] rawTriggerData = FileHandler.readBinary(SCENE_BASE_PATH + this.name + TRIGGER_EXTENSION);
+
+		for(int i=0; i<rawTriggerData.length; i+=4) {
+			this.createTrigger(rawTriggerData[i], rawTriggerData[i + 1], rawTriggerData[i + 2], rawTriggerData[i + 3]);
 		}
 	}
 
@@ -80,6 +109,8 @@ public class Scene {
 			this.tiles[yIndex * SCENE_SIZE + xIndex].setTrigger(trigger);
 		}
 	}
+
+	// endregion
 
 	public Vector2i getSpawn() {
 		return this.spawn;
@@ -101,33 +132,6 @@ public class Scene {
 		int tileIndexY = (int) (yPos / Tile.TILE_SIZE) - 1;
 
 		return (tileIndexX >= 0 && tileIndexX < SCENE_SIZE) && (tileIndexY >= 0 && tileIndexY < SCENE_SIZE);
-	}
-
-	// Modifier byte:
-	// 76543210
-	// ||||||||
-	// |||||||+- 1st bit for the number of rotations
-	// ||||||+-- 2nd bit for the number of rotations
-	// |||||+---
-	// ||||+----
-	// |||+-----
-	// |+-------
-	// +--------
-
-	// Status byte:
-	// 76543210
-	// ||||||||
-	// |||||||+- Can be walked on/through
-	// ||||||+--
-	// |||||+---
-	// ||||+----
-	// |||+-----
-	// |+-------
-	// +--------
-	public Tile generateTile(byte index, byte modifier, byte status, Vector2f position) {
-		Tile tile = new Tile(this.tilemap, index, position, (status & 0b00000001) != 0);
-		tile.rotate90Deg(modifier & 0b00000011);
-		return tile;
 	}
 
 	public void render() {
