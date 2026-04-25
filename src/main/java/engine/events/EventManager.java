28746ee4d489ad384d75fb4e1fd6c14436d91a43
Lucaauem <luca-aussem@t-online.de>
2026-04-25 22:30:25 +0200

    Feature: Simple event manager

---
+++ src/main/java/engine/events/EventManager.java
@@ -0,0 +1,32 @@
+package engine.events;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EventManager {
+  private static EventManager instance;
+
+  private final HashMap<String, ArrayList<Runnable>> subscriptions;
+
+  private EventManager() {
+    this.subscriptions = new HashMap<>();
+  }
+
+  public static EventManager getInstance() {
+    if (instance == null) {
+      instance = new EventManager();
+    }
+    return instance;
+  }
+
+  public void publish(String event) {
+    for (Runnable func : this.subscriptions.get(event)) {
+      func.run();
+    }
+  }
+
+  public void subscribe(String event, Runnable func) {
+    this.subscriptions.putIfAbsent(event, new ArrayList<>());
+    this.subscriptions.get(event).add(func);
+  }
+}
